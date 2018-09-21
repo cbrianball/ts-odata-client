1ce2c350a27df21f043464016ce9207871dbc044
Brian Ball <cbrianball@gmail.com>
2018-09-21 08:28:50 -0400

    Added coments, increased npm version

--- src/lib/ODataV4Context.ts
+++ src/lib/ODataV4Context.ts
@@ -3,7 +3,8 @@ import { ODataContext } from "./ODataContext";
 import { ExcludeProperties } from "./ExcludeProperties";
 
 /**
- * Base ODataContext class; use this class as a base for communicating with services that are compatible with OData v4
+ * Base ODataContext class; use this class as a base for communicating with services that are compatible with OData v4.
+ * This context uses the fetch library; if the runtime environment does not support fetch, please use a polyfill.
  */
 export abstract class ODataV4Context extends ODataContext {
     constructor(basePath: string, protected requestInit?: () => RequestInit) {
