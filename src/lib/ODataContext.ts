af3f02804f36ad2dd9235dd097085f149a367186
Brian Ball <cbrianball@gmail.com>
2022-01-05 17:33:40 -0500

    Allow ODataContext.createQuery to define initial shape of returned entities (#13)
    Co-authored-by: Brian Ball <brian.ball@perficient.com>

--- src/lib/ODataContext.ts
+++ src/lib/ODataContext.ts
@@ -1,4 +1,5 @@
 import { ODataQuery } from "./ODataQuery";
+import type { ExcludeProperties } from "./ExcludeProperties";
 
 /**
  * Base ODataContext class; expected to implement specific versions of OData.
@@ -10,5 +11,5 @@ export abstract class ODataContext {
      * Creates an @type {ODataQuery} instance used to query the provided endpoint.
      * @param endpoint The path, relative to the basePath provided to the contructor.
      */
-    protected abstract createQuery<T>(endpoint: string): ODataQuery<T>;
+    protected abstract createQuery<T, U = ExcludeProperties<T, any[]>>(endpoint: string): ODataQuery<T, U>;
 }
